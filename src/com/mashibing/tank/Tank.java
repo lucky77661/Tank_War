376b295416afbe5b87a4538b1ca8b8acce770ed4
Lucky77661 <770790717@qq.com>
2023-02-16 19:41:49 +0800

    3小节：抽象出tank类，封装相应的属性方法，在TankFrame中直接调用。

---
+++ src/com/mashibing/tank/Tank.java
@@ -0,0 +1,50 @@
+package com.mashibing.tank;
+
+import java.awt.Graphics;
+
+public class Tank {
+	private int x, y;
+	private Dir dir = Dir.DOWN;
+	private static final int SPEED = 10;
+	
+	public Dir getDir() {
+		return dir;
+	}
+
+	public void setDir(Dir dir) {
+		this.dir = dir;
+	}
+
+	
+
+	public Tank(int x, int y, Dir dir) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.dir = dir;
+	}
+
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		
+		g.fillRect(x, y, 50, 50);
+//		x +=10;
+//		y +=10;
+		switch (dir) {
+		case LEFT:
+			x -= SPEED;
+			break;
+		case UP:
+			y -= SPEED;
+			break;
+		case RIGHT:
+			x += SPEED;
+			break;
+		case DOWN:
+			y += SPEED;
+			break;
+		}
+
+	}
+
+}
